eda91c6cbfe6e3af9ee3378b867600a1b55b3cc4
Mukesh-P <132244197+Mukesh-P@users.noreply.github.com>
2024-01-07 19:12:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import streamlit as st
 
 st.set_page_config(page_title="Cetch-up", page_icon="Logo-e.png", layout='wide')
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style.css")
+
 with st.container():
     st.title("Welcome to Cetch-up :wave:")
 
